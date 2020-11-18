b9dcaae5142dfcf4a378cde36bba59cdbd2b5af2
delayedaa <delayedaa@gmail.com>
2020-11-18 14:24:40 -0600

    [index.js] Create two routes to start accessing db

---
+++ index.js
@@ -0,0 +1,19 @@
+let db = require('./models');
+
+// Create
+
+db.user.create({
+  firstName: 'Alan',
+  lastName: 'Avery',
+  age: 36
+}).then(createdUser => {
+  console.log(createdUser);
+});
+
+// Read
+
+db.user.findOne({
+  where: { firstName: 'Alan' }
+}).then(foundUser => {
+  console.log(foundUser);
+});
