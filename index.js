dc0ed12f5b3b83e569c1b3a0827cad48caf33184
delayedaa <delayedaa@gmail.com>
2020-11-18 15:07:25 -0600

    [index.js] Add additional routes to manipulate db data

--- index.js
+++ index.js
@@ -2,20 +2,71 @@ let db = require('./models');
 
 // Create
 
-db.user.create({
-  firstName: 'Alan',
-  lastName: 'Avery',
-  age: 36
-}).then(createdUser => {
-  console.log(createdUser);
-});
+// db.user.create({
+//   firstName: 'Alan',
+//   lastName: 'Avery',
+//   age: 36
+// }).then(createdUser => {
+//   console.log(createdUser);
+// });
 
 // Read
 
-db.user.findOne({
-  where: { firstName: 'Alan' }
-}).then(foundUser => {
-  console.log(foundUser);
-});
+db.user.findOne({ where: { firstName: 'Ryan' } })
+  .then(foundUser => {
+    console.log(foundUser);
+  });
 
-// Next...
+// Find or create
+
+// db.user.findOrCreate({
+//   where: {
+//     firstName: 'Emily',
+//     lastName: 'Harmeyer'
+//   },
+//   defaults: { age: 35 }
+// }).then(([user, created]) => {
+//   console.log(user);
+// });
+
+// db.user.findOrCreate({
+//   where: {
+//     firstName: 'Ryan',
+//   },
+//   defaults: {
+//     lastName: 'Johnson',
+//     age: 35
+//   }
+// }).then(([user, created]) => {
+//   console.log(user);
+// });
+
+// Find all
+
+// db.user.findAll().then(users => {
+//   console.log(users);
+// });
+
+// Update
+
+// db.user.update({
+//   firstName: 'Walter',
+//   lastName: 'Mansilla',
+//   age: 31
+// }, {
+//   where: {
+//     id: 2
+//   }
+// }).then(numRowsChanged => {
+//   console.log(numRowsChanged);
+// });
+
+// Delete
+
+// db.user.destroy({
+//   where: {
+//     id: 3
+//   }
+// }).then(numRowsDeleted => {
+//   console.log(numRowsDeleted);
+// });
